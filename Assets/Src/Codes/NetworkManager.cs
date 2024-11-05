bf50dafdb36b7a05ba5be610c30203eed19b4d54
이상민 <yihs97@naver.com>
2024-11-06 01:39:39 +0900

    feat 9일차 (클라이언트)ping 전송 로직 추가

--- Assets/Src/Codes/NetworkManager.cs
+++ Assets/Src/Codes/NetworkManager.cs
@@ -231,6 +231,9 @@ public class NetworkManager : MonoBehaviour
 
             switch (packetType)
             {
+                case Packets.PacketType.Ping:
+                    HandlePingPacket(packetData);
+                    break; 
                 case Packets.PacketType.Normal:
                     HandleNormalPacket(packetData);
                     break;
@@ -291,4 +294,23 @@ public class NetworkManager : MonoBehaviour
             Debug.LogError($"Error HandleLocationPacket: {e.Message}");
         }
     }
+
+    async void HandlePingPacket(byte[] data) {
+    // 타임스탬프 딱히 필요없음
+    // 바로 돌려줄 예정
+
+        // 헤더 생성
+        byte[] header = CreatePacketHeader(data.Length, Packets.PacketType.Ping);
+
+        // 패킷 생성
+        byte[] packet = new byte[header.Length + data.Length];
+        Array.Copy(header, 0, packet, 0, header.Length);
+        Array.Copy(data, 0, packet, header.Length, data.Length);
+
+        await Task.Delay(GameManager.instance.latency);
+        
+        // 패킷 전송
+        stream.Write(packet, 0, packet.Length);
+    }
+
 }
